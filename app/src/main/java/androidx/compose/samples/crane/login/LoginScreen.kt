67433a2b52774b6a28ba7f19b5dcbf6f7fbb898a
Min-Jae-Bae <qoalswo123@icloud.com>
2022-07-07 16:31:05 +0900

    로그인 UI

--- app/src/main/java/androidx/compose/samples/crane/login/LoginScreen.kt
+++ app/src/main/java/androidx/compose/samples/crane/login/LoginScreen.kt
@@ -1,22 +1,42 @@
 package androidx.compose.samples.crane.login
 
+import android.util.Patterns
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.runtime.*
+import androidx.compose.samples.crane.R
 import androidx.compose.samples.crane.ui.CraneTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.w3c.dom.Text
 
 
 @Composable
 fun LoginScreen() {
+
+
+    val focusManager = LocalFocusManager.current
+
 
     var email by remember {
         mutableStateOf("")
@@ -26,6 +46,21 @@ fun LoginScreen() {
         mutableStateOf("")
     }
 
+    /*유효한지 확인*/
+    val isEmailValid by derivedStateOf {
+        Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    val isPasswordValid by derivedStateOf {
+        password.length > 7
+    }
+
+    var isPasswordVisible by remember {
+        mutableStateOf(false)
+    }
+
+
+
     Column(
         modifier = Modifier
             .background(color = Color.LightGray)
@@ -33,13 +68,117 @@ fun LoginScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        Text(
-            text = "Hello"
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_account_circle_24),
+            contentDescription = "Account Logo",
+            modifier = Modifier.size(150.dp)
 
         )
+
+
+        Text(
+            text = "로그인",
+            fontFamily = FontFamily.Companion.SansSerif,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+
+        )
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
+            shape = RoundedCornerShape(16.dp),
+            border = BorderStroke(1.dp, Color.Black)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(all = 10.dp)
+            ) {
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("이메일 주소") },
+                    placeholder = { Text("abc@domain.com") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                    ),
+                    isError = isEmailValid
+                )
+
+
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("비밀번호") },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                    ),
+                    isError = isPasswordValid
+                )
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+                    enabled = isEmailValid && isPasswordValid
+                ) {
+                    Text(
+                        text = "로그인",
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        fontSize = 16.sp
+                    )
+                }
+            }
+        }
+        Row(
+            horizontalArrangement = Arrangement.End,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            TextButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    color = Color.Black,
+                    fontStyle = FontStyle.Italic,
+                    text = "비밀번호 찾기",
+                    modifier = Modifier.padding(end = 10.dp)
+                )
+            }
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            enabled = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)
+        ) {
+            Text(
+                text = "계정 만들기",
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                fontSize = 16.sp
+            )
+        }
     }
-
-
 }
 
 @Preview(showBackground = true)
